d0a5aa84ac5e7400ce29a29d96a40b21c7af255e
Hasan Mehdi Rizvi <hasanmrizvi72@gmail.com>
2025-06-22 03:08:30 +0530

    feat(dashboard): generate dashboard component and add route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth';
+import { Dashboard } from './pages/dashboard/dashboard';
+
 export const routes: Routes = [
   // When a user visits the main page, show them the AuthComponent
-  { path: '', component: AuthComponent }
+  { path: '', component: AuthComponent },
+
+  // 2. This was the missing route for our dashboard page
+  { path: 'dashboard', component: Dashboard }
 ];
