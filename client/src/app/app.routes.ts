a3bc6fba8ddafc5856308516018d43cdc7b9fe47
Hasan Mehdi Rizvi <hasanmrizvi72@gmail.com>
2025-06-22 01:23:19 +0530

    feat(routing): generate auth component and set as default route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Auth } from './pages/auth/auth';
+export const routes: Routes = [
+  // When a user visits the main page, show them the AuthComponent
+  { path: '', component: Auth}
+];
