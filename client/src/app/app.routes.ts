ac88b59b0fc52cdf83b1afaf2041ef99f5d115a8
Hasan Mehdi Rizvi <hasanmrizvi72@gmail.com>
2025-06-22 01:29:28 +0530

    fix: resolve AuthComponent and environment import errors

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { Auth } from './pages/auth/auth';
+import { AuthComponent } from './pages/auth/auth';
 export const routes: Routes = [
   // When a user visits the main page, show them the AuthComponent
-  { path: '', component: Auth}
+  { path: '', component: AuthComponent }
 ];
