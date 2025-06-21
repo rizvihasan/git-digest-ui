8e5856e0154e0477e6c822840b8e6ca651231c53
Hasan Mehdi Rizvi <hasanmrizvi72@gmail.com>
2025-06-22 01:52:00 +0530

    fix: broken URL

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -3,6 +3,6 @@
 
 export const environment = {
     production: false,
-    supabaseUrl: 'https://msrshsgofzuzegekgojfojh.supabase.co', 
+    supabaseUrl: 'https://msrhsgofzuzgeskgfohj.supabase.co', 
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zcmhzZ29menV6Z2Vza2dmb2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1MzM0MDAsImV4cCI6MjA2NjEwOTQwMH0.2rUMaHa6odpHMsXBy6YsTkzxjJFvQ76C8fdXMWF5xv0'
   };
