e768b71a70fbd4ae3e4de959baa2b6669695d951
Hasan Mehdi Rizvi <hasanmrizvi72@gmail.com>
2025-06-22 02:40:08 +0530

    fix: supabase paths

--- client/src/app/core/supabase.ts
+++ client/src/app/core/supabase.ts
@@ -2,7 +2,7 @@
 
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from 'app/environments/environment.template';
+import { environment } from 'app/environments/environment';
 
 @Injectable({
   providedIn: 'root'
