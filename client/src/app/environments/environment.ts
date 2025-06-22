d833483949f4afc00fa1cf0e4b069fdff4ba1d14
Hasan Mehdi Rizvi <hasanmrizvi72@gmail.com>
2025-06-23 00:03:51 +0530

    fix: minor string fix

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -2,7 +2,7 @@
 // This file is for local development
 
 export const environment = {
-    production: false,
-    supabaseUrl: 'https://msrhsgofzuzgeskgfohj.supabase.co', 
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zcmhzZ29menV6Z2Vza2dmb2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1MzM0MDAsImV4cCI6MjA2NjEwOTQwMH0.2rUMaHa6odpHMsXBy6YsTkzxjJFvQ76C8fdXMWF5xv0'
-  };
+  production: false,
+  supabaseUrl: 'https://msrhsgofzuzgeskgfohj.supabase.co', 
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zcmhzZ29menV6Z2Vza2dmb2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2MTY4NjEsImV4cCI6MjA2NjE5Mjg2MX0.CvUB4SeR7YmEPDs31e-N1tjiaOTlz2TJFImZiOEcNiI'
+  };            
